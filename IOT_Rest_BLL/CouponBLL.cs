2f43d161aa0b3d48f814aed82f29534020e487c5
鑫乐 张 <1045668182@qq.com>
2020-07-16 15:13:14 +0800

    123

---
+++ IOT_Rest_BLL/CouponBLL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dapper;
+using MySql.Data.MySqlClient;
+using IOT_Rest_Model.DBModels;
+using System.Linq;
+
+namespace IOT_Rest_BLL
+{
+    public class CouponBLL
+    {
+        private string Connection = "server=192.168.0.139;User Id=root;password=123456;Database=restaurant";
+
+        public List<tb_Coupon> ShowCoupon()
+        {
+            using (MySqlConnection conn=new MySqlConnection(Connection))
+            {
+                return conn.Query<tb_Coupon>("select * from tb_Coupon").ToList();
+            }
+        }
+    }
+}
